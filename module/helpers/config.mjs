daedc36f106bfd20fc9420b2f82e043b0af2f4d6
MEBonham <programmer@mckaybonham.com>
2021-07-28 13:35:43 -0600

    Initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const ZWOLF = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ ZWOLF.abilities = {
+  "str": "ZWOLF.AbilityStr",
+  "dex": "ZWOLF.AbilityDex",
+  "con": "ZWOLF.AbilityCon",
+  "int": "ZWOLF.AbilityInt",
+  "wis": "ZWOLF.AbilityWis",
+  "cha": "ZWOLF.AbilityCha"
+};
+
+ZWOLF.abilityAbbreviations = {
+  "str": "ZWOLF.AbilityStrAbbr",
+  "dex": "ZWOLF.AbilityDexAbbr",
+  "con": "ZWOLF.AbilityConAbbr",
+  "int": "ZWOLF.AbilityIntAbbr",
+  "wis": "ZWOLF.AbilityWisAbbr",
+  "cha": "ZWOLF.AbilityChaAbbr"
+};
